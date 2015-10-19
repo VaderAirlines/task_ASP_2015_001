0a8448a8fe40e49c37bb7b5720763830b9ce3ae3
VaderAirlines <dennisbellinkx@gmail.com>
2015-10-19 21:00:18 +0200

    first commit
    empty database script added

---
+++ database/db_Nina_skateboarding.sql
@@ -0,0 +1,103 @@
+USE [master]
+GO
+
+/****** Object:  Database [Nina_skateboarding]    Script Date: 10/19/2015 20:55:06 ******/
+CREATE DATABASE [Nina_skateboarding] ON  PRIMARY 
+( NAME = N'Nina_skateboarding', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL10_50.MSSQLSERVER\MSSQL\DATA\Nina_skateboarding.mdf' , SIZE = 3072KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
+ LOG ON 
+( NAME = N'Nina_skateboarding_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL10_50.MSSQLSERVER\MSSQL\DATA\Nina_skateboarding_log.ldf' , SIZE = 1024KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET COMPATIBILITY_LEVEL = 100
+GO
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [Nina_skateboarding].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET ANSI_NULL_DEFAULT OFF 
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET ANSI_NULLS OFF 
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET ANSI_PADDING OFF 
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET ANSI_WARNINGS OFF 
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET ARITHABORT OFF 
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET AUTO_CLOSE OFF 
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET AUTO_CREATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET AUTO_SHRINK OFF 
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET AUTO_UPDATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET CURSOR_DEFAULT  GLOBAL 
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET NUMERIC_ROUNDABORT OFF 
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET QUOTED_IDENTIFIER OFF 
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET RECURSIVE_TRIGGERS OFF 
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET  DISABLE_BROKER 
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET TRUSTWORTHY OFF 
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET PARAMETERIZATION SIMPLE 
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET HONOR_BROKER_PRIORITY OFF 
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET  READ_WRITE 
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET RECOVERY FULL 
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET  MULTI_USER 
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET PAGE_VERIFY CHECKSUM  
+GO
+
+ALTER DATABASE [Nina_skateboarding] SET DB_CHAINING OFF 
+GO
+
